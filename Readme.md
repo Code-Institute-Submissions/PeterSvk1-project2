dcfca3709311a817fe0ab3465aaeefd515b77f95
peter <peter.vlacuha@pobox.sk>
2024-02-25 19:30:18 +0000

    Added credits to readme, with link I used

--- Readme.md
+++ Readme.md
@@ -71,3 +71,12 @@ __Footer__
 - the live link can be found here https://petersvk1.github.io/project2/
 
 ## Credits
+- __Media__
+- picture for rules and and link for wiki was taken from [www.instructables.com](https://www.instructables.com/How-to-Play-Rock-Paper-Scissors-Lizard-Spock/)
+- Picture for a header was taken from google search and edited by me.
+- icons are used from https://kit.fontawesome.com/cd9b9e993c.js
+
+- __Content__
+- Game are was heavily inspired by project: https://github.com/PeterSvk1/Lovemath-Project which was teaching student how it works
+- another website which inspired me and gave me ideas how to work on project and start with java code https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+JSE_PAGPPF+2021_Q2/courseware/30137de05cd847d1a6b6d2c7338c4655/c3bd296fe9d643af86e76e830e1470dd/
+- game logic was inspired by this video: https://www.youtube.com/watch?v=Nb1YRElHVLc , 
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ const userScoreElement = document.getElementById('user');
 const npcScoreElement = document.getElementById('npc');
 const drawScoreElement = document.getElementById('draw');
 const triesLeftElement = document.getElementById('tries');
-const userChoiceWord = document.getElementById('user-choice-word');
-const npcChoiceWord = document.getElementById('npc-choice-word');
+const userWord = document.getElementById('user-choice-word');
+const npcWord  = document.getElementById('npc-choice-word');
 const buttons = document.querySelectorAll('.button');
 
 let userScore = 0;
@@ -22,6 +22,7 @@ function whoWon(userChoice, npcChoice) {
         alert('Its a draw, try again');
         drawScore++;
         drawScoreElement.textContent = drawScore;
+        console.log('draw');
     } else if (
         (userChoice === 'rock' && (npcChoice === 'scissors' || npcChoice === 'lizard')) ||
         (userChoice === 'paper' && (npcChoice === 'rock' || npcChoice === 'spock')) ||
@@ -32,10 +33,12 @@ function whoWon(userChoice, npcChoice) {
         alert('Winner');
         userScore++;
         userScoreElement.textContent = userScore;
+        console.log('player win');
     } else {
         alert('You lose');
         npcScore++;
         npcScoreElement.textContent = npcScore;
+        console.log('player loss');
     }
 
     
@@ -60,25 +63,26 @@ function resetGame() {
         drawScoreElement.textContent = drawScore;
         triesLeftElement.textContent = tries;
 
-        userChoiceWord.textContent = '';
-        npcChoiceWord.textContent = '';
+        userWord.textContent = '';
+        npcWord.textContent = '';
     }, 3000);
 }
 
 function handleButtonClick(event) {
     const userChoice = event.currentTarget.getAttribute('aria-label');
     const npcChoice = npcRandomChoice();
-    console.log('User choice:', userChoice);
-    console.log('NPC choice:', npcChoice);
+    console.log('Player :', userChoice);
+    console.log('NPC :', npcChoice);
 
-    userChoiceWord.textContent = userChoice;
-    npcChoiceWord.textContent = npcChoice;
+    userWord.textContent = userChoice;
+    npcWord.textContent = npcChoice;
 
     whoWon(userChoice, npcChoice);
 
+
     setTimeout(() => {
-        userChoiceWord.textContent = '';
-        npcChoiceWord.textContent = '';
+        userWord.textContent = '';
+        npcWord.textContent = '';
     }, 5000);
 }
 
